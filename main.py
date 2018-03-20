6359b2c194dd3bed2a03cebb3b310b9f80a5d81f
Blake <10bjohnson2@gmail.com>
2018-03-20 17:19:30 -0600

    added horizontal cropping calculations

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
+from PIL import Image
+
+
+def get_horizontal_cropping(rowheight, img):
+    imglist = []
+    left_upper_x = 0
+    left_upper_y = 0
+    right_lower_x = img.width
+    right_lower_y = rowheight
+    remaining = img.height - rowheight
+    print("({0}, {1}, {2}, {3})".format(left_upper_x, left_upper_y, right_lower_x, right_lower_y))
+    imglist.append(img.crop((left_upper_x, left_upper_y, right_lower_x, right_lower_y)))
+
+    while remaining > 0:
+        left_upper_y += rowheight
+        right_lower_y += rowheight
+        remaining -= rowheight
+        if remaining < 0:
+            print("less than 0 - using remaining")
+            print("({0}, {1}, {2}, {3})".format(left_upper_x, left_upper_y, right_lower_x, img.height))
+            imglist.append(img.crop((left_upper_x, left_upper_y, right_lower_x, img.height)))
+            break
+        print("({0}, {1}, {2}, {3})".format(left_upper_x, left_upper_y, right_lower_x, right_lower_y))
+        imglist.append(img.crop((left_upper_x, left_upper_y, right_lower_x, right_lower_y)))
+
+    return imglist
+
+
 def main():
-    pass
+    img = Image.open("mercy.png")
+    imglist = get_horizontal_cropping(200, img)
+
+    for pic in imglist:
+        pic.show()
 
 if __name__ == "__main__":
     # call main fuction
