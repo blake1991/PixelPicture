06644dd99b37a144979d8a865252721e33a0bb49
Blake <10bjohnson2@gmail.com>
2018-03-20 20:32:09 -0600

    added vertical cropping algorithm

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 from PIL import Image
 
 
+def get_vertical_cropping(columnwidth, img):
+    imglist = []
+    left_upper_x = 0
+    left_upper_y = 0
+    right_lower_x = columnwidth
+    right_lower_y = img.height
+    remaining = img.width - columnwidth
+    print("({0}, {1}, {2}, {3})".format(left_upper_x, left_upper_y, right_lower_x, right_lower_y))
+    imglist.append(img.crop((left_upper_x, left_upper_y, right_lower_x, right_lower_y)))
+
+    while remaining > 0:
+        left_upper_x += columnwidth
+        right_lower_x += columnwidth
+        remaining -= columnwidth
+        if remaining < 0:
+            print("less than 0 - using remaining")
+            print("({0}, {1}, {2}, {3})".format(left_upper_x, left_upper_y, img.width, right_lower_y))
+            imglist.append(img.crop((left_upper_x, left_upper_y, img.width, right_lower_y)))
+            break
+        print("({0}, {1}, {2}, {3})".format(left_upper_x, left_upper_y, right_lower_x, right_lower_y))
+        imglist.append(img.crop((left_upper_x, left_upper_y, right_lower_x, right_lower_y)))
+
+    return imglist
+
+
 def get_horizontal_cropping(rowheight, img):
     imglist = []
     left_upper_x = 0
@@ -28,12 +53,14 @@ def get_horizontal_cropping(rowheight, img):
 
 def main():
     img = Image.open("mercy.png")
-    imglist = get_horizontal_cropping(200, img)
+    # imglist = get_horizontal_cropping(200, img)
+    imglist = get_vertical_cropping(720, img)
 
     for pic in imglist:
         pic.show()
 
+
 if __name__ == "__main__":
-    # call main fuction
+    # call main function
     main()
     exit(0)
